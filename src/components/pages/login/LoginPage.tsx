8af7f67c92277dbc288d87797e43c3735045db03
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-09-15 16:49:27 +0200

    🚧 Login : Add logic to the form

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -2,21 +2,38 @@ import { useState } from "react";
 
 export default function LoginPage() {
   const [user, setUser] = useState({
-    name : "",
-    password : "",
-  })
+    name: "",
+    password: "",
+  });
+  useNavigate()
+
+  const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
+    event.preventDefault()
+
+  }
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
-    
-  }
-  return ( 
+    const { name, value } = event.target;
+    setUser((prevUser) => ({ ...prevUser, [name]: value }));
+  };
+  return (
     <div>
       <div className="container">
-        <form>
+        <form onSubmit={handleSubmit}>
           <label>Nom</label>
-          <input name="name" value={user.name} onChange={handleChange} placeholder="Nom" />
+          <input
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+            placeholder="Nom"
+          />
           <label>Mot de passe</label>
-          <input name="password" value={user.password} onChange={handleChange} placeholder="mot de passe" />
+          <input
+            name="password"
+            value={user.password}
+            onChange={handleChange}
+            placeholder="mot de passe"
+          />
           <button>Valider</button>
         </form>
       </div>
