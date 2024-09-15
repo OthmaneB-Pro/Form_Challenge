3d3721c2c4c5bd56e4d443774905ee502958ad7c
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-09-15 17:03:04 +0200

    💄 Login : Add first style

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,24 +1,25 @@
 import { useState } from "react";
 import { useNavigate } from "react-router-dom";
+import styled from "styled-components";
 
 export default function LoginPage() {
   const [user, setUser] = useState({
     name: "",
     password: "",
   });
-  const navigate = useNavigate()
+  const navigate = useNavigate();
 
   const handleSubmit = (event: React.FormEvent<HTMLFormElement>) => {
-    event.preventDefault()
-    navigate(`/form2/${user.name}`)
-  }
+    event.preventDefault();
+    navigate(`/form2/${user.name}`);
+  };
 
   const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const { name, value } = event.target;
     setUser((prevUser) => ({ ...prevUser, [name]: value }));
   };
   return (
-    <div>
+    <LoginPageStyled>
       <div className="container">
         <form onSubmit={handleSubmit}>
           <label>Nom</label>
@@ -38,6 +39,28 @@ export default function LoginPage() {
           <button>Valider</button>
         </form>
       </div>
-    </div>
+    </LoginPageStyled>
   );
 }
+
+const LoginPageStyled = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+
+  .container {
+    width: 400px;
+    height: 400px;
+    background-color: green;
+  }
+  form {
+    display: flex;
+    flex-direction: column;
+    justify-content: center;
+    align-items: center;
+  }
+  input {
+    padding: 5px;
+    margin-top: 10px;
+  }
+`;
