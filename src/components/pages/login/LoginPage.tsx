d08c8dd2c0e093cae5e38dc44e2ac62caf98ab55
OthmaneB-Pro <othmane93.baar@gmail.com>
2024-09-15 16:43:47 +0200

    ✨ Login : Introduce static form

--- src/components/pages/login/LoginPage.tsx
+++ src/components/pages/login/LoginPage.tsx
@@ -1,8 +1,25 @@
+import { useState } from "react";
+
 export default function LoginPage() {
-  return (
+  const [user, setUser] = useState({
+    name : "",
+    password : "",
+  })
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>) => {
+    
+  }
+  return ( 
     <div>
-      <div className="container">LoginPage</div>
+      <div className="container">
+        <form>
+          <label>Nom</label>
+          <input name="name" value={user.name} onChange={handleChange} placeholder="Nom" />
+          <label>Mot de passe</label>
+          <input name="password" value={user.password} onChange={handleChange} placeholder="mot de passe" />
+          <button>Valider</button>
+        </form>
+      </div>
     </div>
   );
 }
-
